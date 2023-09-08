1f5f13ae5d208df0fb82fcc61a91f8583f39ded4
unknown <sharmavimal7467@gmail.com>
2023-09-08 14:14:50 +0530

    information about variable

---
+++ class-2nd/script.js
@@ -0,0 +1,14 @@
+console.log("hello js ! .😎😁")
+console.log("i'm starting to learn js.")
+console.log("witout log,console will not work");
+console.log("container is used to consume some data which we use further!.")
+console.log("js allow to change while run time.")
+let a = 67;
+console.log(a);
+a = "hahahaha";
+console.log(a);
+console.log("if we write two different variable it work as normal")
+console.log("Name convension of js")
+console.log("1-Letters,Digits,Underscores & $ sign are allow but it not start with any number.")
+console.log("js reserved word cannot be used as variable name")
+console.log("js is case sensitive language ---> suggested never use like this vimal/ViMal")
